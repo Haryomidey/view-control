b8a4d41fedae125ca1490a6b1b646ff8bba2e344
Haryomidey <oladiipoayomide2021@gmail.com>
2026-05-02 15:28:25 +0100

    [FIX] publish runtime package types

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,6 @@ import App from './App.tsx';
 import 'react-loading-skeleton/dist/skeleton.css';
 import './index.css';
 
-import { init } from '@viewcontrol/runtime';
-
-init({
-  projectId: 'vc_f0d4a5a08be75e60c30d072c',
-  apiUrl: 'https://view-control-production.up.railway.app',
-  debug: false
-});
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
